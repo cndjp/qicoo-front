c202f3ff7b8e53a1826dbe527f1eb7c56f90c9e2
translucens <mail@w-st.com>
2018-10-17 01:46:49 +0900

    モバイルデバイスとPCでデザインを分離した
    close #4

--- src/components/QuestionForm.tsx
+++ src/components/QuestionForm.tsx
@@ -23,18 +23,18 @@ class QuestionForm extends React.Component<Prop, State> {
 
     public render() {
         return (
-        <nav className="navbar navbar-expand-md navbar-dark fixed-bottom">
+        <nav className="navbar navbar-dark fixed-bottom pt-2 px-2 pb-0">
             <form onSubmit={this.postNewQuestion}
-                className="form-inline mt-2 mt-md-0 w-100">
-
-                <div className="flex-grow-1 w-75 mr-2">
-                    <input value={this.state.input}
+                className="form-inline w-100">
+                <div className="mb-2 pr-md-0 col-md">
+                    <textarea value={this.state.input}
                     onChange={this.handleQuestionInput}
-                    className="form-control w-100" type="text" placeholder="聞いてみよう：生命、宇宙、そして万物についての究極の疑問の答えは何？" />
+                    className="form-control w-100"
+                    rows={2}
+                    placeholder="聞いてみよう：生命、宇宙、そして万物についての究極の疑問の答えは何？" />
                 </div>
-                <div className="flex-grow-0">
-                
-                <button className="btn btn-outline-light my-2 my-sm-0" type="submit">聞いてみる</button>
+                <div className="mb-2 col-md-auto">
+                    <button className="btn btn-outline-light w-100" type="submit">聞いてみる</button>
                 </div>
             </form>
         </nav>
@@ -48,7 +48,7 @@ class QuestionForm extends React.Component<Prop, State> {
         this.clearForm();
     }
 
-    private handleQuestionInput = (e: React.FormEvent<HTMLInputElement>) : void => {
+    private handleQuestionInput = (e: React.FormEvent<HTMLTextAreaElement>) : void => {
         this.setState(
             {input: e.currentTarget.value}
         );
