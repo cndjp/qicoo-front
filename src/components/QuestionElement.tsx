6a403988271dde81f277ffd231563cb568e28e6e
nnao45 <n4sekai5y@gmail.com>
2019-11-26 20:11:22 +0900

    buttunを調整

--- src/components/QuestionElement.tsx
+++ src/components/QuestionElement.tsx
@@ -33,7 +33,7 @@ export class QuestionElement extends React.Component<Props, State> {
         >
           <div className="d-flex w-100 justify-content-between">
             <h5 className="mb-1">{q.comment}</h5>
-            <div className="btn-group btn-group-sm">
+            <div className="btn-groups">
               <button
                 type="button"
                 className="btn btn-info mr-2"
